d0d21d5ffde7b38fffb9c72ce146c457a8700fde
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-12 20:00:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(n)
-  (1..n).each do |i|
-    if i % 3 == 0 && i % 5 == 0
+    if n % 3 == 0 && n % 5 == 0
       puts 'fizzbuzz'
-    elsif i % 3 == 0
+    elsif n % 3 == 0
       puts 'fizz'
-    elsif i % 5 == 0
+    elsif n % 5 == 0
       puts 'buzz'
     else
       puts i
